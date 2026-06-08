db00c2eae20eccc396a451502ca015e6e071ec1b
joshikumar8230 <joshikumar8230@gmail.com>
2026-06-08 08:37:17 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 // Initialize Groq client 
 const client = new OpenAI({
-  apiKey: "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",
+  apiKey: "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",
   baseURL: "https://api.groq.com/openai/v1"
 });
 const JWT_SECRET = "OpenAi";
